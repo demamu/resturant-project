855584b21501804842373467d295a3065a75aa0f
Daniel <demamu@outlook.com>
2020-04-15 00:19:58 -0500

    create menu model in the frontend with coreresponding crud operations

---
+++ FrontEnd/src/models/menu.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+
+export default class Menu {
+  constructor(name, price, cal, imgUrl, review, rating) {
+    this.name = name || 'Chicken Frice';
+    this.price = price || 4.99;
+    this.calories = cal || 345;
+    this.imgUrl = imgUrl || 'Chicken Frice';
+    this.review = review || 'this is a good meal...';
+    this.rating = rating || 4.4;
+  }
+
+  static async add(menu){
+    let uri = axios.defaults.baseURL + '/menu';
+    let response = await axios.post(uri, menu);
+    console.log(response);
+    return response.data;
+  }
+
+  static async getAll(){
+    let uri = axios.defaults.baseURL + '/menu';
+    let response = await axios.get(uri);
+    console.log(response);
+    return response.data;
+  }
+
+  static async getById(id){
+    let uri = axios.defaults.baseURL + '/menu/' + id;
+    let response = await axios.get(uri);
+    console.log(response);
+    return response.data;
+  }
+  static async update(id, menu){
+    let uri = axios.defaults.baseURL + '/menu/' + id;
+    let response = await axios.put(uri, menu);
+    console.log(response);
+    return response.data;
+  }
+  static async remove(id){
+    let uri = axios.defaults.baseURL + '/menu/' + id;
+    let response = await axios.delete(uri);
+    console.log(response);
+    return response.data;
+  }
+}
